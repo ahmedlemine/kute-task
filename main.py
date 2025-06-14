628efc4b06eae439bee119e54867f92f89a9eafd
Ahmed Lemine <ahmed.lemine@yahoo.com>
2025-06-14 23:00:06 +0400

    add Flet app as first step to integrate Flet

---
+++ main.py
@@ -0,0 +1,101 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Kute Task"
+
+    # must be in the same order as page_views routes
+    routes = ["/", "/list", "/new"]
+
+    # Side Drawer
+    def handle_dismissal(e):
+        pass
+
+    def handle_change(e):
+        page.go(routes[e.control.selected_index])
+        page.close(drawer)
+
+    def show_drawer(e):
+        page.drawer.open = True
+        page.drawer.update()
+
+    drawer = ft.NavigationDrawer(
+        on_dismiss=handle_dismissal,
+        on_change=handle_change,
+        controls=[
+            ft.Container(height=12),
+            ft.NavigationDrawerDestination(
+                label="Single Task",
+                icon=ft.Icons.EXPLORE,
+                selected_icon=ft.Icon(ft.Icons.EXPLORE_OUTLINED),
+            ),
+            ft.Divider(thickness=2),
+            ft.NavigationDrawerDestination(
+                icon=ft.Icon(ft.Icons.LIST_OUTLINED),
+                label="List Tasks",
+                selected_icon=ft.Icons.LIST,
+            ),
+            ft.NavigationDrawerDestination(
+                icon=ft.Icon(ft.Icons.CREATE_OUTLINED),
+                label="Add Task",
+                selected_icon=ft.Icons.CREATE,
+            ),
+        ],
+    )
+    page.drawer = drawer
+
+
+    # used by route_change() to set the view matching the route
+    page_views = {
+        "/": ft.View(
+            "/",
+            [
+                drawer,
+                ft.AppBar(
+                    title=ft.Text("Focus"),
+                    bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+                ),
+            ],
+        ),
+        "/list": ft.View(
+            "/list",
+            [
+                drawer,
+                ft.AppBar(
+                    title=ft.Text("Task List"),
+                    bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+                ),
+                ft.ElevatedButton("Back", on_click=lambda _: page.go("/")),
+            ],
+        ),
+        "/new": ft.View(
+            "/new",
+            [
+                drawer,
+                ft.AppBar(
+                    title=ft.Text("New Task"),
+                    bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+                ),
+                ft.ElevatedButton("Back", on_click=lambda _: page.go("/")),
+            ],
+        ),
+    }
+
+    def route_change(route):
+        page.views.clear()
+        page.views.append(page_views[page.route])
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.scroll = ft.ScrollMode.ADAPTIVE
+
+ft.app(main, view=ft.AppView.WEB_BROWSER)
